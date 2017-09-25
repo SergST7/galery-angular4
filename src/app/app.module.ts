a6468b36effd1b2b3c90cef9eaf5a0532c5bf4ad
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-09-25 23:57:47 +0300

    added navbar, galery and image components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GaleryComponent } from './galery/galery.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GaleryComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
